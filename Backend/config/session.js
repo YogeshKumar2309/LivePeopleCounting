ac8b5e9c3b491001ffb561848a2a8507a6d67b33
YogeshKumar2309 <placementsbyyogesh@gmail.com>
2025-09-20 17:27:59 +0530

    feat: setup session middleware with secure, sameSite and MongoStore for always-login

--- Backend/config/session.js
+++ Backend/config/session.js
@@ -1,4 +1,5 @@
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,8 +8,14 @@ export const sessionMiddleware =  session({
     secret: process.env.SESSION_SECRET || "default_secret",
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      ttl: 1000 * 60 * 60 * 24 * 365,
+    }),
     cookie: {
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24 * 365,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
     },
   });
